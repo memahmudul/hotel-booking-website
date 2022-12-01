0dee01efc135afca8623976ec39e1c91f1ef9a62
memahmudul <farcrymahmudul@gmail.com>
2022-12-01 13:01:46 +0600

    gallery section done

--- js/script.js
+++ js/script.js
@@ -15,3 +15,21 @@ var swiper = new Swiper(".home-slider", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+var swiper = new Swiper(".gallarySwiper", {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
